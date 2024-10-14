5bf7b06cb1b5ae880c375ee925d106b38a8e3d0a
Edward Ruzek <edward.ruzek@gmail.com>
2024-10-14 14:36:41 -0400

    adding pop and top methods and adding simple tests for stack

--- arrays/stack.go
+++ arrays/stack.go
@@ -8,10 +8,17 @@ type Stack[T any] struct {
 }
 
 //TODO: refactor! this is super hacky
-func Push[T any](s Stack[T], val T) Stack[T] {
+func Push[T any](s Stack[T], val T) {
     tVal := []T{val}
     s.Vals = append(tVal, s.Vals...)
-    return s
 }
 
+func Top[T any](s Stack[T]) T {
+    return s.Vals[0]
+}
 
+func Pop[T any](s Stack[T]) T {
+    val := s.Vals[0]
+    s.Vals = s.Vals[1:len(s.Vals)-1]
+    return val
+}
--- arrays/stack_test.go
+++ arrays/stack_test.go
@@ -7,10 +7,39 @@ import (
 func PushTest(t *testing.T) {
     var s Stack[int]
 
-    s = Push(s, 1)
-
+    Push(s, 1)
     if s.Vals[0] != 1 {
         t.Fatalf("val expected 1 received %v", s.Vals[0])
+    }       
+}
+
+
+func TopTest(t *testing.T) {
+    var s Stack[int]
+    Push(s, 2)
+    Push(s, 3)
+    Push(s, 4)
+    Push(s, 5)
+
+    v := Top(s)
+
+    if v != 2 {
+        t.Fatalf("val expected 2 received %v", v)
     }
-        
+}
+
+func PopTest(t *testing.T) {
+    var s Stack[int]
+    Push(s, 2)
+    Push(s, 3)
+    Push(s, 4)
+    Push(s, 5)
+
+    v := Pop(s)
+
+    if v != 2 {
+        t.Fatalf("val expected 2 received %v", v)
+    } else if s.Vals[0] != 3 {
+        t.Fatalf("val expected 3 received %v", s.Vals[0])
+    }
 }
